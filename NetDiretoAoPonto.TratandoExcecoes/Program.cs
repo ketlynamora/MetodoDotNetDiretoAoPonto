cd3710ea8c6816fb4639589ccb008c73691761bb
Ketlyn Amora <ketypricila70@gmail.com>
2021-09-30 08:30:16 -0300

    "Realizado capítulo de tratamento de exceções"

---
+++ NetDiretoAoPonto.TratandoExcecoes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NetDiretoAoPonto.TratandoExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tratando Exceções
+
+            var seteString = "sete";
+            string valorNull = null;
+            var longValue = long.MaxValue.ToString();
+
+            try
+            {
+                var formatException = int.Parse(seteString);
+                var argumentNullException = int.Parse(valorNull);
+                var overflowException = int.Parse(longValue);
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine($"Format Exception: {exception.Message}");
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine($"Argument Null Exception: {exception.Message}");
+            }
+            catch (OverflowException exception)
+            {
+                Console.WriteLine($"Overflow Exception: {exception.Message}");
+            }
+            finally
+            {
+                Console.WriteLine($"Esse código é executado sempre! ");
+            }
+
+            #endregion
+        }
+    }
+}
