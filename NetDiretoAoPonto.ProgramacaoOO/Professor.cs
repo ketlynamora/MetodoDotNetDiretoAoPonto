f662210cf38c01d9780414e45bcd43a2632762cc
Ketlyn Amora <ketypricila70@gmail.com>
2021-10-02 17:50:55 -0300

    "Realizado capitulo ProgramacaoOO"

---
+++ NetDiretoAoPonto.ProgramacaoOO/Professor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetDiretoAoPonto.ProgramacaoOO
+{
+    public class Professor : Pessoa
+    {
+        public Professor(decimal salario, string nome, string documento, DateTime dataNascimento) :
+            base (nome, documento, dataNascimento)
+        {
+            Salario = salario;
+            Turmas = new List<string> { "A", "B" };
+        }
+
+        public List<string> Turmas { get; private set; }
+        public decimal Salario { get; private set; }
+
+        public override void SeApresentar()
+        {
+            base.SeApresentar();
+
+            var turmas = string.Join(',', Turmas);
+            Console.WriteLine($"Meu salário é {Salario}, e ensino para as turmas {turmas}");
+        }
+    }
+}
